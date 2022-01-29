88301d7c77d8baa12f3d27b348beec62fc62450b
derbenev <derbenev.vs@gmail.com>
2022-01-29 19:42:39 +0300

    5 Этап

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 let counterQuestions = 0;
-let questionNumber = 0; // вставить рандомное значение
+let questionNumber;
+let questionNumberTotal = 7;
 let resultUser = 0;
+
+let questionNumberRound =() => {questionNumber =Math.round(Math.random() * ((mas.length-1) - 0) + 0) // вставить рандомное значение
+return questionNumber}; 
 
 let startFunction = () => {
 document.querySelector(".index_button").remove()
 startTest();
 };
 
-let startTest = () => {
-   
-    if(counterQuestions > 0){
-            
-            document.querySelector(".centerQuestions").remove();
-            document.querySelector(".answersQuestions").remove();
-            document.querySelector(".questionsTimeTest").remove();
-            document.querySelector(".questionsTimeTest").remove();
-            document.querySelector(".header_bottom").remove();
+questionNumberRound();
 
-            if(document.querySelector(".index_button") != null){
-                document.querySelector(".index_button").remove();
-            };
+let startTest = () => { // Вывод вопроса
+
+  // startFunction();
+
+  if(mas.length <= 0){
+    return resultTotal();
+  };
+
+  removalBlock();
+  questionsTest();
+   
+  counterQuestions++;
+  mas.splice(questionNumber, 1);
+  questionNumberRound();
+
     };
-    
-    questionsTest();
-   
-    counterQuestions++;
-    questionNumber++;
 
-};
-
-let questionsTest = () => {
+let questionsTest = () => { // создание вопраса (верстка, классы)
 
 let centerQuestions = document.createElement("div");
 let answersQuestions = document.createElement("div");
 let buttonQuestions = document.createElement("button");
-
 
 centerQuestions.classList.add("centerQuestions");
 answersQuestions.classList.add("answersQuestions");
@@ -48,12 +48,17 @@ centerQuestions.innerHTML = mas[questionNumber].questionTest;
 
 if(mas[questionNumber].choiceAnswer == false){
     answersQuestions.innerHTML = mas[questionNumber].answerUser;
-    document.querySelector(".center").append(buttonQuestions);
+    // document.querySelector(".center").append(buttonQuestions);
 
     for(let cardAnswer of document.querySelectorAll(".answerCard")){
-    cardAnswer.onclick = myControl;
+    cardAnswer.onclick = borderRed;
+    cardAnswer.addEventListener("mouseenter", borderBlue); // переработать
+    cardAnswer.addEventListener("mouseleave", borderBlueNull); // переработать
+
+    cardAnswer.addEventListener("click", startTest);
     };
-  
+    
+
 }else if(mas[questionNumber].choiceAnswer == true){
      
    answersQuestions.innerHTML = '<input id="numbInput" class="answerUserInput" type="text"><button class="index_button_2" onclick="myFunction(); startTest()">Следующий вопрос</button>';
@@ -70,19 +75,21 @@ function myFunction() { // переработать, вставить прове
     var x;
     x = document.getElementById("numbInput").value;
     console.log(x);
-    console.log("Привет!");
+};
 
-  }
+function borderRed(){ // красная рамка при клике
+  this.classList.add("answerCardBorder");
+};
 
-  function myControl(){ // переработать, вставить проверку
-    console.log("Привет!");
-      this.classList.add("answerCardBorder");
+function borderBlue(){ // синяя рамка при наведении
+  this.classList.add("answerCardBorder_2");
+};
 
-      };
+function borderBlueNull(){ // убирает синюю рамку при наведении
+  this.classList.remove("answerCardBorder_2");
+};
 
-
-let questionsCounter = () => {
-
+let questionsCounter = () => { // функци с счетчиком вопросов. таймер - добавить.
 
     let headerBottom = document.createElement("div");
     headerBottom.classList.add("header_bottom");
@@ -97,5 +104,28 @@ let questionsCounter = () => {
     document.querySelector(".header_bottom").append(questionsNumber);
     document.querySelector(".header_bottom").append(timeTest);
 
-    questionsNumber.innerHTML = `<p>Вопрос ${questionNumber+1} из ${mas.length}</p>`;
+    questionsNumber.innerHTML = `<p>Вопрос ${counterQuestions+1} из ${questionNumberTotal}</p>`;
+};
+
+let removalBlock = () => { // Удаляет блоки
+  if(counterQuestions > 0){
+            
+    document.querySelector(".centerQuestions").remove();
+    document.querySelector(".answersQuestions").remove();
+    document.querySelector(".questionsTimeTest").remove();
+    document.querySelector(".questionsTimeTest").remove();
+    document.querySelector(".header_bottom").remove();
+  
+    if(document.querySelector(".index_button") != null){
+        document.querySelector(".index_button").remove();
+    };
+};
+};
+let resultTotal = () => { // Вывод результата
+  removalBlock();
+  console.log("Итог!");
+  let centerQuestions = document.createElement("div");
+  centerQuestions.classList.add("centerQuestions");
+  document.querySelector(".center").append(centerQuestions);
+  centerQuestions.innerHTML = `<p>${masResult[0].textResult}</p>`; // Внести условие 
 };
---
+++ js/masResult.js
@@ -0,0 +1,14 @@
+let masResult = [
+    {
+        textResult: '<p>Привет 1</p>'
+    },
+    {
+        textResult: '<p>Привет 2</p>'
+    },
+    {
+        textResult: '<p>Привет 3</p>'
+    },
+    {
+        textResult: '<p>Привет 4</p>'
+    },
+];
